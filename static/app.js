66d46b5d3160097185bc3d462a8860f6cd618449
John Wood <johna613@gmail.com>
2023-03-13 12:14:15 -0400

    js oop refactor complete

--- static/app.js
+++ static/app.js
@@ -2,91 +2,98 @@ class BoggleGame {
 	constructor (time = 60){
 		this.time = time;
 		this.score = 0;
-		this.seenWords = []
-		this.canPlay = true
-		this.intervalID =this.setInterval(timer, 1000)
+		this.seenWords = [];
+		this.canPlay = true;
+		this.timer = this.timer.bind(this);
+		this.intervalID = setInterval(this.timer, 1000);
 	}
 	/* sends word to server to evaluate*/
 	async submitWord(evt){
-		evt.preventDefault()
-		if (!this.canPlay){return}
+		evt.preventDefault();
+		if (!this.canPlay){return};
 		const word = ($('input').val()).toLowerCase();
 		
-		if(this.alreadyGuessed(word)){return}
+		if(word.length < 1) {
+			this.generateAlert('danger', 'No Word Was Entered', 500)
+			return
+		};
+
+		if(this.alreadyGuessed(word)){return};
 		
-		const response = await axios.post('/check', { params: {'word' : word}})
-		const result = response.data.result
-		this.showResult(result, word)
-		$('input').val('')
+		const response = await axios.post('/check', { params: {'word' : word}});
+		const result = response.data.result;
+		this.showResult(result, word);
+		$('input').val('');
 	}
 	/** updates highscore and number of plays on server */
 	async updateHighscoreAndNumPlays(score){
-		const response = await axios.post('/score', { params: {'new_score' : score}})
-		const highScore = response.data.high_score
-		const numOfPlays = response.data.num_plays
-		this.updateInfo('num-plays', 'GAME#', numOfPlays)
-		this.updateInfo('high-score', 'HIGHSCORE', highScore)
+		const response = await axios.post('/score', { params: {'new_score' : score}});
+		const highScore = response.data.high_score;
+		const numOfPlays = response.data.num_plays;
+		this.updateInfo('num-plays', 'GAME#', numOfPlays);
+		this.updateInfo('high-score', 'HIGHSCORE', highScore);
 	}
 	/**checks if word has already been guessed */
 	alreadyGuessed(word){
 		if (this.seenWords.includes(word)){
-			this.generateAlert('info',`"${word}" has already been guessed`, 500)
-			$('input').val('')
-			return true
+			this.generateAlert('info',`"${word}" has already been guessed`, 500);
+			$('input').val('');
+			return true;
 		} else{
-			this.seenWords.push(word)
-			return false
+			this.seenWords.push(word);
+			return false;
 		}
 	}
 	/** generates an alert showing feedback on guess*/
 	showResult(result, word){
 		if(result === 'ok'){
-			const wordLen = word.length
+			const wordLen = word.length;
 			if(wordLen < 2){
-				this.generateAlert('success',`${wordLen} POINT`, 300)
+				this.generateAlert('success',`${wordLen} POINT`, 300);
 			}
 			else {
-				this.generateAlert('success',`${wordLen} POINTS`, 300)
+				this.generateAlert('success',`${wordLen} POINTS`, 300);
 			}
-			this.calculateScore(word)
+			this.calculateScore(word);
 		}
 		else if(result === 'not-on-board'){
-			this.generateAlert('danger',`"${word}" not on board`, 500)
+			this.generateAlert('danger',`"${word}" not on board`, 500);
 		}
 		else if(result === 'not-word'){
-			this.generateAlert('warning', `"${word}" is not a word`, 500)
+			this.generateAlert('warning', `"${word}" is not a word`, 500);
 		}
 	}
 	/**keeps track of current game's score */
 	calculateScore(word){
-		const word_score = word.length
-		this.score += word_score
-		this.updateInfo('score', 'SCORE', this.score)
+		const word_score = word.length;
+		this.score += word_score;
+		this.updateInfo('score', 'SCORE', this.score);
 	}
 	/**game timer */
 	timer(){
-		this.time -= 1
-		this.updateInfo('timer', 'TIME', this.time)
+		this.time -= 1;
+		this.updateInfo('timer', 'TIME', this.time);
 		if(this.time === 0){
-			this.canPlay = false
-			clearInterval(this.intervalID)
-			this.updateHighscoreAndNumPlays(this.score)
+			this.canPlay = false;
+			clearInterval(this.intervalID);
+			this.updateHighscoreAndNumPlays(this.score);
 		}
 	}
 	/** helper function to generate game info html */
 	updateInfo(id, text, val){
-		$(`#${id}`).html(`${text}: ${val}`)
+		$(`#${id}`).html(`${text}: ${val}`);
 	}
 	/** helper function to generate HTML for alerts */
 	generateAlert(type,msg,time){
 		$('#alert-container').append(
 			`<div id="alert" class="alert alert-${type}" role="alert">${msg}</div>`).css('display', 'block'
 		);
-		setTimeout(() => $('#alert').remove(), time)
+		setTimeout(() => $('#alert').remove(), time);
 	}
 }
 
-$('#submit').on('click', submitWord);
+const boggleGame1 = new BoggleGame;
+$('#submit').on('click', (evt) => boggleGame1.submitWord(evt));
 
 // *****************************************************************
 // const intervalID = setInterval(timer, 1000)
